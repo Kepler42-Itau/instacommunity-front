b468aca5f8e0bf08a6cdfe4c406f61a12db3e77a
SidBertalia <sidbertalia@gmail.com>
2021-11-12 18:30:37 -0300

    feat:add register button on home page

---
+++ pages/home/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { Button, ButtonGroup, Heading, Center, Link } from "@chakra-ui/react";
+import { ArrowForwardIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+import { useRouter } from 'next/router'
+
+
+const Home: NextPage = () => {
+	const router = useRouter()
+	return (
+		<div>
+		<Head>
+			<title>Instacommunity</title>
+		</Head>
+		<Center>
+			<Button rightIcon={<ArrowForwardIcon />} colorScheme="teal" variant="outline" onClick={() => router.push('/register')}>
+				Register
+			</Button>
+		</Center>
+		</div>
+	);
+};
+
+export default Home;
