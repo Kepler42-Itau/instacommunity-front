e392fd1024db3e11402a128a5361f1c2834d1b8a
hde-oliv <henriquerocha@tutanota.com>
2021-12-16 13:25:36 -0300

    refactor: refactor navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,30 +5,21 @@ import {
   Flex,
   Input,
   Text,
-  Link as Link2,
   InputGroup,
   Avatar,
   InputRightElement,
-  useColorMode, ScaleFade, Slide, SlideFade
+  useColorMode,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import React, { useState } from "react";
-
-import {SearchIcon, MoonIcon, SunIcon, AddIcon} from "@chakra-ui/icons";
+import { useState } from "react";
+import { SearchIcon, MoonIcon, SunIcon, AddIcon } from "@chakra-ui/icons";
 
 interface NavBarProps {
-  profile: Boolean;
-  home: Boolean;
   searchFunction?: Function;
 }
 
-export default function NavBar({
-  profile = false,
-  home = false,
-  searchFunction = undefined,
-}: NavBarProps) {
-  const [searchTerm, setSearchTerm] = React.useState("");
+export default function NavBar({ searchFunction = undefined }: NavBarProps) {
+  const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
   const { colorMode, toggleColorMode } = useColorMode();
 
@@ -52,28 +43,28 @@ export default function NavBar({
   return (
     <Box>
       <Flex p="1%" justifyContent="center">
-          <Flex flex="1" justifyContent="start" ml="auto">
-            <Text
-                  bgGradient='linear(to-r, #FF7900, #9D4EDD)'
-                  bgClip='text'
-                  height="100%"
-                  fontSize='3xl'
-                  fontWeight='extrabold'
-                  userSelect="none"
-                  cursor="pointer"
-                  _hover={{
-                    bgGradient: "linear(to-r, #ff6d00, #7B2CBF)",
-                  }}
-                  _active={{
-                    transform: 'scale(0.98)',
-                  }}
-                  transition='all 0.2s ease-in-out'
-                  onClick={() => router.push('/')}
-                >
-                  InstaCommunity
-                </Text>
+        <Flex flex="1" justifyContent="start" ml="auto">
+          <Text
+            bgGradient="linear(to-r, #FF7900, #9D4EDD)"
+            bgClip="text"
+            height="100%"
+            fontSize="3xl"
+            fontWeight="extrabold"
+            userSelect="none"
+            cursor="pointer"
+            _hover={{
+              bgGradient: "linear(to-r, #ff6d00, #7B2CBF)",
+            }}
+            _active={{
+              transform: "scale(0.98)",
+            }}
+            transition="all 0.2s ease-in-out"
+            onClick={() => router.push("/")}
+          >
+            InstaCommunity
+          </Text>
         </Flex>
-       <Center flex="5" justifyContent="center" alignSelf="center">
+        <Center flex="5" justifyContent="center" alignSelf="center">
           <form
             style={{ width: "50%", margin: "0 auto" }}
             onSubmit={handleSubmit}
@@ -87,34 +78,38 @@ export default function NavBar({
                 borderRadius="md"
                 border="1px"
               />
-              <InputRightElement
-                children={
-                  <Button type="submit" size="md">
-                    <SearchIcon />
-                  </Button>
-                }
-              />
+              <InputRightElement>
+                <Button type="submit" size="md">
+                  <SearchIcon />
+                </Button>
+              </InputRightElement>
             </InputGroup>
           </form>
         </Center>
-          <Flex flex="1" justifyContent="end" mr="auto">
-            <Center mr="5%">
-                  <Button onClick={() => toggleColorMode()} width="100%">
-                    {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                  </Button>
-            </Center>
-            <Center mr="5%">
-              <Button
-                colorScheme="orange"
-                onClick={() => router.push("/communities/create")}
-                pd="5%"
-                width="100%"
-              >
-                Criar Comunidade
-              </Button>
-            </Center>
-            <Avatar name='Ada' size="md" cursor="pointer" userSelect="none" onClick={() => router.push("/user/1") }/>
-          </Flex>
+        <Flex flex="1" justifyContent="end" mr="auto">
+          <Center mr="5%">
+            <Button onClick={() => toggleColorMode()} width="100%">
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+          </Center>
+          <Center mr="5%">
+            <Button
+              colorScheme="orange"
+              onClick={() => router.push("/communities/create")}
+              pd="5%"
+              width="100%"
+            >
+              Criar Comunidade
+            </Button>
+          </Center>
+          <Avatar
+            name="Ada"
+            size="md"
+            cursor="pointer"
+            userSelect="none"
+            onClick={() => router.push("/user/1")}
+          />
+        </Flex>
       </Flex>
     </Box>
   );
