53a3342cea55a71f3836beafe0b50ef83499c1b1
hde-oliv <henriquerocha@tutanota.com>
2022-01-18 17:21:00 -0300

    refactor: make new User model

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,11 @@
-export default interface User {
-  id: Number;
+interface User {
+  id: string;
   name: string;
+  occupation: string | null;
+  description: string | null;
+  email: string;
+  contact: string | null;
+  photoURL: string | null;
 }
+
+export default User;
