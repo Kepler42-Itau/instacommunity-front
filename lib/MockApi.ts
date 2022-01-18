40c979a7a7e6195d335895842cc1081431a46582
hde-oliv <henriquerocha@tutanota.com>
2022-01-18 18:53:50 -0300

    feat: add MockApi files for json-server

---
+++ lib/MockApi.ts
@@ -0,0 +1,23 @@
+import Community from "../models/Community";
+import User from "../models/User";
+import ErrorResponse from "../models/ErrorResponse";
+
+export const getFollowedCommunities = async (
+  userId: string
+): Promise<Community[]> => {
+  const res = await fetch(`http://localhost:8080/followedCommunities`, {
+    method: "GET",
+    headers: [["Content-Type", "application/json"]],
+  });
+  if (res.ok) return res.json();
+  else {
+    return new Promise(() => []);
+  }
+};
+
+export const getUser = async (uid: string): Promise<User | ErrorResponse> => {
+  const res = await fetch(`http://localhost:8080/user/user1`, {
+    method: "GET",
+  });
+  return res.json();
+};
