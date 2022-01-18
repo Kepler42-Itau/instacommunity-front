49924d976b220bcce71b0bc7e3457db0e8812123
hde-oliv <henriquerocha@tutanota.com>
2022-01-18 17:23:03 -0300

    feat: create Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,54 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import { UserContext } from "../lib/UserContext";
+import { useEffect, useState } from "react";
+import { NextRouter, useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, checkAdmin, logoutFromGoogle } from "../lib/Firebase";
+import Head from "next/head";
+import theme from "../lib/Theme";
+import User from "../models/User";
+import { Box, Container, ScaleFade } from "@chakra-ui/react";
+import API from "../services/api";
+
+interface MainProps {
+  router: NextRouter;
+}
+
+const Main: React.FC<MainProps> = ({ children, router }) => {
+  const [user] = useAuthState(auth);
+  const [userBackend, setUserBackend] = useState(null || User);
+
+  useEffect(() => {
+    if (user != null) {
+      API.getUser(user.uid).then((res) => {
+        setUserBackend(res);
+      });
+    } else {
+      setUserBackend(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return (
+    <Box>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <UserContext.Provider value={{ user, userBackend, setUserBackend }}>
+        <ScaleFade key={router.route} in={true}>
+          <Head>
+            <link rel="icon" href="/favicon.ico" />
+            <title>InstaCommunity</title>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+          </Head>
+          <Container maxW="container.xl" centerContent>
+            {children}
+          </Container>
+        </ScaleFade>
+      </UserContext.Provider>
+    </Box>
+  );
+};
+
+export default Main;
