8362b9e373542d4137e74c73c574a8f2d3ed71bf
hde-oliv <henriquerocha@tutanota.com>
2022-01-18 17:20:38 -0300

    feat: configure new UserContext

--- lib/UserContext.ts
+++ lib/UserContext.ts
@@ -1,8 +1,15 @@
 import { createContext } from "react";
+import { User } from "firebase/auth";
+import userBackend from "../models/User";
 
 interface UserCtx {
-  user: null | any,
-  userBackend: null | any
+  user: null | User | undefined;
+  userBackend: null | userBackend;
+  setUserBackend: Function;
 }
 
-export const UserContext = createContext({ user: null,  userBackend: null } as UserCtx);
+export const UserContext = createContext({
+  user: null,
+  userBackend: null,
+  setUserBackend: Function,
+} as UserCtx);
