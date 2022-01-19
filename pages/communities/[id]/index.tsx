28c99c9c3e6d8ccdbb795c7147304476a7533054
hde-oliv <henriquerocha@tutanota.com>
2022-01-19 12:05:30 -0300

    refactor: restrucutre of CommunityPage

--- pages/communities/[id]/index.tsx
+++ pages/communities/[id]/index.tsx
@@ -1,45 +1,35 @@
-import type { NextPage } from "next";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-
-import React, { useState, useEffect } from "react";
-
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import { Box, Link } from "@chakra-ui/react";
-import { HStack, VStack, Center, Heading, Text, Grid, GridItem, Flex } from "@chakra-ui/react";
+import { NextRouter, useRouter } from "next/router";
+import { useState, useEffect, useContext } from "react";
 import {
-  List,
-  ListItem,
-  ListIcon,
-  OrderedList,
-  UnorderedList,
+  Center,
+  Heading,
+  Text,
+  Flex,
+  Button,
+  Box,
+  Avatar,
+  useToast,
 } from "@chakra-ui/react";
-import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
-import { LinkIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import { useToast } from "@chakra-ui/react";
-
 import NavBar from "../../../components/NavBar";
-import ContactModal from "../../../components/ContactModal";
-
 import User from "../../../models/User";
-import UserCommunity from "../../../models/UserCommunity";
-import api from "../../../services/api";
+import Contact from "../../../models/Contact";
+import Community from "../../../models/Community";
+import {
+  followCommunity,
+  unfollowCommunity,
+  getCommunity,
+  getCommunityFollowers,
+} from "../../../lib/MockApi";
+import UserContext from "../../../lib/UserContext";
 
-const Community: NextPage = (props: any) => {
+const CommunityPage = ({
+  community,
+  followers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const toast = useToast();
-  const followers = props.list;
-  const [showFollowers, setShowFollowers] = useState(false);
-  const [isFollowing, setIsFollowing] = useState(false);
-
   const router = useRouter();
-  const { id } = router.query;
-  const userId = router.query.userId || 1;
-  const name = "Ada";
-  const contacts = [
-    props.data.contact,
-    props.data.contact2,
-    props.data.contact3,
-  ];
 
   const handleToast = (ok: Boolean) => {
     if (!ok) {
@@ -53,156 +43,155 @@ const Community: NextPage = (props: any) => {
     }
   };
 
-  const handleFollowClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-
-    const userId = router.query.userId?.toString()!;
-    const communityId = id?.toString()!;
-    api.followCommunity(communityId, userId).then((res) => {
-      const ok = "id" in res;
-      if (ok) setIsFollowing(true);
-      handleToast(ok);
-    });
-  };
-
-  const handleUnfollowClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    const userId = router.query.userId?.toString()!;
-    const communityId = id?.toString()!;
-    api.unFollowCommunity(communityId, userId).then((res) => {
-      const ok = "id" in res;
-      if (ok) setIsFollowing(false);
-      handleToast(false);
-    });
-  };
-
-  const handleFollowerClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    setShowFollowers(!showFollowers);
-  };
-
-  useEffect(() => {
-    console.log("User id is ", userId);
-    if (followers.some((follower: User) => follower.id === userId)) {
-      setIsFollowing(true);
-    }
-  }, []);
-
   return (
     <Box>
       <Head>
-        <title>{props.data.name}</title>
+        <title>{community.name}</title>
       </Head>
       <Flex flexDirection="column" height="100vh">
         <NavBar />
-        <Box
-          mt="3%"
-          p="10%"
-          pt="2%"
-          pb="0%"
-          justifyContent="end"
-          border="px"
-          borderColor="gray.100"
-        >
-          <Box>
-            <Flex
-              boxShadow="xl"
-              p="6%"
-              pt="2%"
-              pb="2%"
-              bg="white"
-              rounded="lg"
-              borderRadius="lg"
-              flexDirection="column"
-            >
-              <Center width="100%">
-                <Avatar mr="auto" name={props.data.name} size="2xl" />
-                <Heading fontSize="6xl" ml="auto" mr="auto">
-                  {props.data.name}
-                </Heading>
-                <ContactModal id={id} ml="auto" contacts={contacts} />
-                {isFollowing ? (
-                  <Button
-                    title="Deixar de seguir"
-                    colorScheme="blue"
-                    variant="outline"
-                    ml="1%"
-                    size="lg"
-                    onClick={handleUnfollowClick}
-                  >
-                    Seguindo
-                  </Button>
-                ) : (
-                  <Button
-                    title="Seguir comunidade"
-                    colorScheme="blue"
-                    size="lg"
-                    ml="1%"
-                    onClick={handleFollowClick}
-                  >
-                    Seguir
-                  </Button>
-                )}
-              </Center>
-              <Box
-                mt="3%"
-                p="10%"
-                pt="2%"
-                pb="2%"
-                justifyContent="end"
-                boxShadow="base"
-                bg="gray.50"
-                rounded="lg"
-                borderRadius="lg"
-              >
-                <Text textAlign="center" fontSize="2xl">
-                  {props.data.description}
-                </Text>
-              </Box>
-              <Flex>
-                <Box flex="2" mt="2%" mr="auto">
-                  {contacts.filter((contact) => {return contact != ""}).map((contact: string, index: number) => (
-                    <Button
-                      key={index}
-                      colorScheme="blue"
-                      size="lg"
-                      mr="2%"
-                      onClick={() => window.open(`${contact}`, "_blank")}
-                    >
-                      Contato {index + 1}
-                    </Button>
-                  ))}
-                </Box>
-                {/* <Grid templateColumns="repeat(4, 1fr)" columnGap="1%" rowGap="1%" bg="gray" flex="1" m="auto" ml="auto">
-                  {followers.map((follower: User, index: number) => (
-                    <Grid key={index}>
-                      <Avatar name={follower.name} size="md" />
-                    </Grid>
-                  ))}
-                </Grid> */}
-              </Flex>
-            </Flex>
-          </Box>
-        </Box>
+        <Flex>
+          <CommunityItem
+            community={community}
+            followers={followers}
+            router={router}
+            toast={handleToast}
+          />
+        </Flex>
       </Flex>
     </Box>
   );
 };
 
-import { GetServerSideProps } from "next";
+interface CommunityItemProps {
+  community: Community;
+  followers: User[];
+  router: NextRouter;
+  toast: Function;
+}
+
+const CommunityItem = ({
+  community,
+  followers,
+  router,
+  toast,
+}: CommunityItemProps) => {
+  const { id } = router.query;
+  const { userBackend } = useContext(UserContext);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    if (followers.some((follower: User) => follower.id === userBackend?.id)) {
+      setIsFollowing(true);
+    }
+  }, []);
+
+  const handleFollowClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const communityId = id?.toString()!;
+    followCommunity(communityId, userBackend?.id).then((res) => {
+      const ok = "id" in res;
+      if (ok) setIsFollowing(true);
+      toast(ok);
+    });
+  };
+
+  const handleUnfollowClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const communityId = id?.toString()!;
+    unfollowCommunity(communityId, userBackend?.id).then((res) => {
+      const ok = "id" in res;
+      if (ok) setIsFollowing(false);
+      toast(false);
+    });
+  };
+
+  return (
+    <Box mt="3%" p="10%" pt="2%" pb="0%" justifyContent="end">
+      <Flex
+        boxShadow="xl"
+        p="6%"
+        pt="2%"
+        pb="2%"
+        bg="white"
+        rounded="lg"
+        borderRadius="lg"
+        flexDirection="column"
+      >
+        <Center width="100%">
+          <Avatar mr="auto" name={community.name} size="2xl" />
+          <Heading fontSize="6xl" ml="auto" mr="auto">
+            {community.name}
+          </Heading>
+          {isFollowing ? (
+            <Button
+              title="Deixar de seguir"
+              colorScheme="blue"
+              variant="outline"
+              ml="1%"
+              size="lg"
+              onClick={handleUnfollowClick}
+            >
+              Seguindo
+            </Button>
+          ) : (
+            <Button
+              title="Seguir comunidade"
+              colorScheme="blue"
+              size="lg"
+              ml="1%"
+              onClick={handleFollowClick}
+            >
+              Seguir
+            </Button>
+          )}
+        </Center>
+        <Box
+          mt="3%"
+          p="10%"
+          pt="2%"
+          pb="2%"
+          justifyContent="end"
+          boxShadow="base"
+          bg="gray.50"
+          rounded="lg"
+          borderRadius="lg"
+        >
+          <Text textAlign="center" fontSize="2xl">
+            {community.description}
+          </Text>
+        </Box>
+        <Flex>
+          <Box flex="2" mt="2%" mr="auto">
+            {community.contacts.map((contact: Contact, index: number) => (
+              <Button
+                key={index}
+                colorScheme="blue"
+                size="lg"
+                mr="2%"
+                onClick={() => window.open(`${contact.link}`, "_blank")}
+              >
+                {contact.title}
+              </Button>
+            ))}
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const id = context.params?.id;
-  if (id == null) return { props: {} };
-  const data = await api.getCommunityPage(id as string);
-  const list = await api.getCommunitiesFollowers(id as string);
+  const community = await getCommunity(id as string);
+  const followers = await getCommunityFollowers(id as string);
 
   return {
     props: {
-      data,
-      list,
+      community,
+      followers,
     },
   };
 };
 
-export default Community;
+export default CommunityPage;
