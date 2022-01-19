67d412ddbc7826db741fae54db3c54c0afa6dcdc
hde-oliv <henriquerocha@tutanota.com>
2022-01-19 11:03:41 -0300

    feat: add getCommunities on MockApi

--- lib/MockApi.ts
+++ lib/MockApi.ts
@@ -16,8 +16,19 @@ export const getFollowedCommunities = async (
 };
 
 export const getUser = async (uid: string): Promise<User | ErrorResponse> => {
-  const res = await fetch(`http://localhost:8080/user/user1`, {
+  const res = await fetch(`http://localhost:8080/users/user1`, {
     method: "GET",
   });
   return res.json();
+};
+
+export const getCommunities = async (): Promise<Community[]> => {
+  const res = await fetch(`http://localhost:8080/communities`, {
+    method: "GET",
+    headers: [["Content-Type", "application/json"]],
+  });
+  if (res.ok) return res.json();
+  else {
+    return new Promise(() => []);
+  }
 };
