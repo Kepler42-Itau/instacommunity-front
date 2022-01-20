30f1cea121e1ea5fe1be1d1626195e6f2dd8e459
hde-oliv <henriquerocha@tutanota.com>
2022-01-20 09:48:55 -0300

    refactor: CommunityPage

--- pages/communities/[id].tsx
+++ pages/communities/[id].tsx
@@ -4,6 +4,7 @@ import { NextRouter, useRouter } from "next/router";
 import { useState, useEffect, useContext } from "react";
 import {
   Center,
+  AvatarGroup,
   Heading,
   Text,
   Flex,
@@ -12,17 +13,18 @@ import {
   Avatar,
   useToast,
 } from "@chakra-ui/react";
-import NavBar from "../../../components/NavBar";
-import User from "../../../models/User";
-import Contact from "../../../models/Contact";
-import Community from "../../../models/Community";
+import NavBar from "../../components/NavBar";
+import User from "../../models/User";
+import Contact from "../../models/Contact";
+import Community from "../../models/Community";
+import SettingsModal from "../../components/SettingsModal";
 import {
   followCommunity,
   unfollowCommunity,
   getCommunity,
   getCommunityFollowers,
-} from "../../../lib/MockApi";
-import UserContext from "../../../lib/UserContext";
+} from "../../lib/MockApi";
+import UserContext from "../../lib/UserContext";
 
 const CommunityPage = ({
   community,
@@ -44,20 +46,18 @@ const CommunityPage = ({
   };
 
   return (
-    <Box>
+    <Box maxW="100vw" width="100%" mx="auto" px={{ base: 2, sm: 12, md: 17 }}>
       <Head>
         <title>{community.name}</title>
       </Head>
-      <Flex flexDirection="column" height="100vh">
-        <NavBar />
-        <Flex>
-          <CommunityItem
-            community={community}
-            followers={followers}
-            router={router}
-            toast={handleToast}
-          />
-        </Flex>
+      <NavBar />
+      <Flex pt="2%" pr="10%" pl="10%" pb="5%">
+        <CommunityItem
+          community={community}
+          followers={followers}
+          router={router}
+          toast={handleToast}
+        />
       </Flex>
     </Box>
   );
@@ -89,40 +89,54 @@ const CommunityItem = ({
   const handleFollowClick = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     const communityId = id?.toString()!;
-    followCommunity(communityId, userBackend?.id).then((res) => {
-      const ok = "id" in res;
-      if (ok) setIsFollowing(true);
-      toast(ok);
+    followCommunity(communityId, userBackend?.id as string).then((res) => {
+      if (res) setIsFollowing(true);
+      toast(res);
     });
   };
 
   const handleUnfollowClick = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     const communityId = id?.toString()!;
-    unfollowCommunity(communityId, userBackend?.id).then((res) => {
-      const ok = "id" in res;
-      if (ok) setIsFollowing(false);
-      toast(false);
+    unfollowCommunity(communityId, userBackend?.id as string).then((res) => {
+      if (res) setIsFollowing(false);
+      toast(res);
     });
   };
 
   return (
-    <Box mt="3%" p="10%" pt="2%" pb="0%" justifyContent="end">
-      <Flex
-        boxShadow="xl"
-        p="6%"
-        pt="2%"
-        pb="2%"
-        bg="white"
-        rounded="lg"
-        borderRadius="lg"
-        flexDirection="column"
-      >
-        <Center width="100%">
-          <Avatar mr="auto" name={community.name} size="2xl" />
+    <Flex
+      width="100%"
+      boxShadow="xl"
+      p="5%"
+      bg="white"
+      rounded="lg"
+      borderRadius="lg"
+      flexDirection="column"
+    >
+      <Center justifyContent="center" width="100%">
+        <Flex flex="1" justifyContent="start" ml="auto">
+          <Avatar
+            mr="auto"
+            name={community.name}
+            src={community.photoURL as string}
+            size="2xl"
+          />
+        </Flex>
+        <Flex flex="5" justifyContent="center" alignSelf="center">
           <Heading fontSize="6xl" ml="auto" mr="auto">
             {community.name}
           </Heading>
+        </Flex>
+        {userBackend?.id === community.admin && (
+          <SettingsModal
+            id={id}
+            ml="auto"
+            contacts={community.contacts}
+            community={community}
+          />
+        )}
+        <Flex flex="1" justifyContent="end" mr="auto">
           {isFollowing ? (
             <Button
               title="Deixar de seguir"
@@ -145,38 +159,62 @@ const CommunityItem = ({
               Seguir
             </Button>
           )}
-        </Center>
-        <Box
-          mt="3%"
-          p="10%"
-          pt="2%"
-          pb="2%"
-          justifyContent="end"
-          boxShadow="base"
-          bg="gray.50"
-          rounded="lg"
-          borderRadius="lg"
-        >
-          <Text textAlign="center" fontSize="2xl">
-            {community.description}
-          </Text>
-        </Box>
-        <Flex>
-          <Box flex="2" mt="2%" mr="auto">
-            {community.contacts.map((contact: Contact, index: number) => (
-              <Button
-                key={index}
-                colorScheme="blue"
-                size="lg"
-                mr="2%"
-                onClick={() => window.open(`${contact.link}`, "_blank")}
-              >
-                {contact.title}
-              </Button>
-            ))}
-          </Box>
         </Flex>
+      </Center>
+      <Box
+        mt="3%"
+        p="10%"
+        pt="2%"
+        pb="2%"
+        justifyContent="end"
+        boxShadow="base"
+        rounded="lg"
+        borderRadius="lg"
+      >
+        <Text textAlign="center" fontSize="2xl">
+          {community.description}
+        </Text>
+      </Box>
+      <Flex mt="4%" mr="2%" ml="2%" justifyContent="center">
+        <ContactBox contacts={community.contacts} />
+        <FollowersBox followers={followers} />
       </Flex>
+    </Flex>
+  );
+};
+
+interface FollowersBoxProps {
+  followers: User[];
+}
+
+const FollowersBox = ({ followers }: FollowersBoxProps) => {
+  return (
+    <AvatarGroup size="md" max={3}>
+      {followers.map((user: User, index: number) => (
+        <Avatar key={index} name={user.name} src={user.photoURL as string} />
+      ))}
+    </AvatarGroup>
+  );
+};
+
+interface ContactBoxProps {
+  contacts: Contact[];
+}
+
+const ContactBox = ({ contacts }: ContactBoxProps) => {
+  return (
+    <Box flex="2">
+      {contacts.map((contact: Contact, index: number) => (
+        <Button
+          key={index}
+          colorScheme="blue"
+          size="lg"
+          mr="2%"
+          onClick={() => window.open(`${contact.link}`, "_blank")}
+        >
+          {contact.title}
+        </Button>
+      ))}
     </Box>
   );
 };
