baa74900bf649ac2272cb111975e330adf5f4f70
hde-oliv <henriquerocha@tutanota.com>
2022-01-24 07:14:43 -0300

    refactor: UserSettingsPage

--- pages/user/settings.tsx
+++ pages/user/settings.tsx
@@ -1,125 +1,216 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { UserContext } from "../../lib/UserContext"
-import React, {useContext, useState} from "react";
+import UserContext from "../../lib/UserContext";
+import { useContext } from "react";
 import NavBar from "../../components/NavBar";
-import {Input, Box, Flex, VStack, Center, FormLabel, FormControl, Avatar, Text, Badge, Switch} from "@chakra-ui/react";
+import { Box, Flex, Heading, Center, Button } from "@chakra-ui/react";
+import { Formik, Form, FormikHelpers } from "formik";
+import { FieldBox, TextareaFieldBox } from "../../components/FieldBox";
 
-const Settings: NextPage  = () => {
-  const {user, userBackend} = useContext(UserContext)
-  // @ts-ignore
+const UserSettingsPage = () => {
+  const { userBackend } = useContext(UserContext);
+
   return (
-    (!userBackend ? <FirstLogin user={user}/> : <ChangeInfo />)
+    <Box maxW="100vw" width="100%" mx="auto" px={{ base: 2, sm: 12, md: 17 }}>
+      <NavBar />
+      <Flex
+        flexDirection="column"
+        pt="2%"
+        pr={{ base: "2%", xl: "10%" }}
+        pl={{ base: "2%", xl: "10%" }}
+        pb="5%"
+      >
+        <Heading
+          mt="2%"
+          fontSize="3xl"
+          textAlign={{ base: "center", md: "left" }}
+        >
+          Configurações do Usuário
+        </Heading>
+        {!userBackend ? <FirstLogin /> : <ChangeInfo />}
+      </Flex>
+    </Box>
   );
 };
 
-function FirstLogin({ user }: any) {
-  const [nickname, setName] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [jobPost, setJobPost] = useState("");
-  const [usePhoto, setUsePhoto] = useState(false);
-
-  const handleJobPostChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setJobPost(e.target.value)
-  }
-  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value)
-  }
-  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value)
-  }
-  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-     setName(e.target.value)
-  }
-  const handlePhotoChange = (e: any) => {
-    setUsePhoto(!usePhoto);
-  }
-  const handleSubmit = () => {
-
-  }
-
-  return (<>
-  <Head>
-    <title>Criar Usuário</title>
-  </Head>
-    <NavBar/>
-      <Flex>
-        <Center mt="3%" width="100%">
-            <VStack spacing="13px" width="40%">
-              <Flex borderRadius="md" pt={2} pb={2} p={6} boxShadow="base">
-                <Flex mb="3%" >
-                  { !usePhoto && <Avatar size='lg' src={user.photoURL} />}
-                  { usePhoto && <Avatar size='lg' name={`${firstName} ${lastName}`} />}
-                <Box ml='3'>
-                  <Text fontWeight='bold'>
-                    {nickname}
-                    <Badge ml='1' colorScheme='green'>
-                       PREVIEW
-                    </Badge>
-                  </Text>
-                  <Text fontSize='sm'>{jobPost}</Text>
-                </Box>
-              </Flex>
-              </Flex>
-
-              {/*<form onSubmit={handleSubmit} width="100%">*/}
-
-              <FormControl id="First name" isRequired>
-                <FormLabel>Nome</FormLabel>
-                <Input
-                  placeholder="Nome"
-                  value={firstName}
-                  width="100%"
-                  onChange={handleFirstNameChange}
-                />
-              </FormControl>
-                <FormControl id="Last name" isRequired>
-                  <FormLabel>Sobrenome</FormLabel>
-                  <Input
-                    placeholder="Sobrenome"
-                    value={lastName}
-                    width="100%"
-                    onChange={handleLastNameChange}
-                  />
-                </FormControl>
-                <FormControl id="nickname" isRequired>
-                <FormLabel>Apelido</FormLabel>
-                <Input
-                  placeholder="Apelido"
-                  value={nickname}
-                  width="100%"
-                  onChange={handleNicknameChange}
-                />
-                </FormControl>
-                <FormControl id="jobpost" isRequired>
-                <FormLabel>Cargo</FormLabel>
-                <Input
-                  placeholder="Cargo"
-                  value={jobPost}
-                  width="100%"
-                  onChange={handleJobPostChange}
-                />
-                </FormControl>
-              <FormControl>
-                <FormLabel>
-                  Usar a foto do Google?
-                </FormLabel>
-                <Switch id="photo" onChange={handlePhotoChange} />
-              </FormControl>
-              {/*</form>*/}
-            </VStack>
-        </Center>
-      </Flex>
-  </>);
+interface UserSettingsValues {
+  name: string;
+  username: string;
+  occupation?: string;
+  description?: string | null;
+  contactLink: string | null;
+  contactTitle: string | null;
+  photoURL?: string | null;
 }
 
-function ChangeInfo() {
-  return (<></>);
-}
+const ChangeInfo = () => {
+  const { userBackend } = useContext(UserContext);
 
-function SettingsForm() {
-  return (<></>);
-}
+  return (
+    <Formik
+      initialValues={{
+        name: userBackend?.name as string,
+        username: userBackend?.username as string,
+        occupation: userBackend?.occupation as string,
+        description: userBackend?.description as string,
+        contactLink: userBackend?.contact?.link as string,
+        contactTitle: userBackend?.contact?.title as string,
+        photoURL: userBackend?.photoURL as string,
+      }}
+      onSubmit={(
+        values: UserSettingsValues,
+        { setSubmitting, resetForm }: FormikHelpers<UserSettingsValues>
+      ) => {
+        setTimeout(() => {
+          // TODO: Implement a real handleSubmit
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      {(props) => (
+        <Form>
+          <FieldBox
+            name={"name"}
+            title={"Nome"}
+            isRequired={true}
+            placeholder={"Ex: Fausto Silva"}
+          />
+          <FieldBox
+            name={"photoURL"}
+            title={"Link da imagem de perfil"}
+            isRequired={false}
+            placeholder={"Ex: https://i.imgur.com/WZIYQW0.png"}
+          />
+          <FieldBox
+            name={"occupation"}
+            title={"Ocupação"}
+            isRequired={false}
+            placeholder={"Ex: Apresentador"}
+          />
+          <Flex>
+            <FieldBox
+              name={"contactLink"}
+              title={"Link do contato"}
+              isRequired={false}
+              placeholder={"Ex: aka.ms/fausto_silva"}
+            />
+            <Flex mr="1%" ml="1%" />
+            <FieldBox
+              name={"contactTitle"}
+              title={"Título do contato"}
+              isRequired={true}
+              placeholder={"Ex: Grupo do WhatsApp"}
+            />
+          </Flex>
+          <TextareaFieldBox
+            name={"description"}
+            title={"Description"}
+            isRequired={false}
+            placeholder={"Ex: Oloco bicho!"}
+          />
+          <Center mt="4%" mb="2%" width="100%">
+            <Button colorScheme="red" type="reset" mr="5%">
+              Cancel
+            </Button>
+            <Button
+              colorScheme="teal"
+              isLoading={props.isSubmitting}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Center>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
-export default Settings;
+const FirstLogin = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        username: "",
+        occupation: "",
+        description: "",
+        contactLink: "",
+        contactTitle: "",
+        photoURL: "",
+      }}
+      onSubmit={(
+        values: UserSettingsValues,
+        { setSubmitting, resetForm }: FormikHelpers<UserSettingsValues>
+      ) => {
+        setTimeout(() => {
+          // TODO: Implement a real handleSubmit
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      {(props) => (
+        <Form>
+          <FieldBox
+            name={"name"}
+            title={"Nome"}
+            isRequired={true}
+            placeholder={"Ex: Fausto Silva"}
+          />
+          <FieldBox
+            name={"username"}
+            title={"Nome de usuário"}
+            isRequired={true}
+            placeholder={"Ex: f_silva"}
+          />
+          <FieldBox
+            name={"photoURL"}
+            title={"Link da imagem de perfil"}
+            isRequired={false}
+            placeholder={"Ex: https://i.imgur.com/WZIYQW0.png"}
+          />
+          <FieldBox
+            name={"occupation"}
+            title={"Ocupação"}
+            isRequired={false}
+            placeholder={"Ex: Apresentador"}
+          />
+          <Flex>
+            <FieldBox
+              name={"contactLink"}
+              title={"Link do contato"}
+              isRequired={false}
+              placeholder={"Ex: aka.ms/fausto_silva"}
+            />
+            <Flex mr="1%" ml="1%" />
+            <FieldBox
+              name={"contactTitle"}
+              title={"Título do contato"}
+              isRequired={true}
+              placeholder={"Ex: Grupo do WhatsApp"}
+            />
+          </Flex>
+          <TextareaFieldBox
+            name={"description"}
+            title={"Description"}
+            isRequired={false}
+            placeholder={"Ex: Oloco bicho!"}
+          />
+          <Center mt="4%" mb="2%" width="100%">
+            <Button colorScheme="red" type="reset" mr="5%">
+              Cancel
+            </Button>
+            <Button
+              colorScheme="teal"
+              isLoading={props.isSubmitting}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Center>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default UserSettingsPage;
